ceacaaaae41d2474adfb89c42eb49a0262abf4f6
[maro-webengineer] <[abridge.rsunabe@gmail.com]>
2025-11-07 19:54:29 +0900

    JavaScriptで書いた配列の要素をハードコーディングで指定してレンダリング

--- README.md
+++ README.md
@@ -9,6 +9,22 @@ Reactの学習過程をまとめたリポジトリです。
 - Reactの基本文法を理解する。
 - 学習の過程を可視化し、成長記録として残す。
 
+
+## 学び
+
+tsxでは以下の構成で書くことができる
+```javascript:tsx
+function App() {
+  // JavaScriptのコードを書く
+  
+  return (
+   /* HTMLで画面の見た目部分を書く */
+  )
+}
+
+export default App
+```
+
 ## 参考資料
 
 https://qiita.com/Sicut_study/items/afd66cac978f4b0a6e61
--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,57 @@ import './App.css'
 
 function App() {
 
+  const defaultMovieList = [
+    {
+      id: 1,
+      name: "君の名は",
+      image:
+        "https://media.themoviedb.org/t/p/w300_and_h450_bestv2/yLglTwyFOUZt5fNKm0PWL1PK5gm.jpg",
+      overview:
+        "1ヵ月後に1000年ぶりの彗星が訪れる日本。東京で暮らす平凡な男子高校生・瀧と、山深い村で都会の生活に憧れながら憂鬱な日々を送る女子高校生・三葉。つながりのない2人は、互いが入れ替わる不思議な夢を見る。",
+    },
+    {
+      id: 2,
+      name: "ハウルの動く城",
+      image:
+        "https://media.themoviedb.org/t/p/w300_and_h450_bestv2/v0K2e1t6ocUNnkZ9BeiFdcOT9LG.jpg",
+    },
+    {
+      id: 3,
+      name: "もののけ姫",
+      image:
+        "https://media.themoviedb.org/t/p/w300_and_h450_bestv2/mVdz3vlmioKWZaHTGfu99zIuayZ.jpg",
+    },
+    {
+      id: 4,
+      name: "バック・トゥ・ザ・フューチャー",
+      image:
+        "https://media.themoviedb.org/t/p/w300_and_h450_bestv2/oHaxzQXWSvIsctZfAYSW0tn54gQ.jpg",
+    },
+  ];
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <div>
+        <p>{defaultMovieList[0].name}</p>
+        <img src={defaultMovieList[0].image} alt={defaultMovieList[0].name} />
+        <p>{defaultMovieList[0].overview}</p>
+      </div>
+      <div>
+        <p>{defaultMovieList[1].name}</p>
+        <img src={defaultMovieList[1].image} alt={defaultMovieList[1].name} />
+        <p>{defaultMovieList[1].overview}</p>
+      </div>
+      <div>
+        <p>{defaultMovieList[2].name}</p>
+        <img src={defaultMovieList[2].image} alt={defaultMovieList[2].name} />
+        <p>{defaultMovieList[2].overview}</p>
+      </div>
+      <div>
+        <p>{defaultMovieList[3].name}</p>
+        <img src={defaultMovieList[3].image} alt={defaultMovieList[3].name} />
+        <p>{defaultMovieList[3].overview}</p>
+      </div>
     </div>
   )
 }

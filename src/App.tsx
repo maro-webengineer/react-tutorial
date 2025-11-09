6a99c361c011cb2cd32c062b3aec7ad2bd42baa1
[maro-webengineer] <[abridge.rsunabe@gmail.com]>
2025-11-09 09:19:31 +0900

    リファクタリング

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import MovieCart from "./MovieCard";
 
 type Movie = {
   id: number;
@@ -99,26 +100,7 @@ function App() {
         </h2>
         <div className="movie-row-scroll">
           {movieList.map((movie) => (
-            <a
-              key={movie.id}
-              href={`/movies/${movie.id}`}
-              className="movie-card"
-            >
-              <div className="movie-card__imgwrap">
-                <img
-                  src={
-                    movie.poster_path
-                      ? `https://image.tmdb.org/t/p/w300_and_h450_bestv2${movie.poster_path}`
-                      : "https://via.placeholder.com/300x450?text=No+Image"
-                  }
-                  alt={movie.original_title}
-                  className="movie-card__image"
-                />
-                <div className="movie-card__overlay">
-                  <h3 className="movie-card__title">{movie.original_title}</h3>
-                </div>
-              </div>
-            </a>
+            <MovieCart key={movie.id} movie={movie} />
           ))}
         </div>
       </section>
